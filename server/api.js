3113e088346b1e94dbac10b56e5439f24645baff
karthik raji <51771000+karthikraji2020@users.noreply.github.com>
2020-10-12 23:56:51 +0530

    Update api.js

--- server/api.js
+++ server/api.js
@@ -2,7 +2,8 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const Hacker = require('./models/hacker');
-
+const nodemailer = require('nodemailer');
+const $env = process.env;
 router.get('/hackers', (req, res) => {
   Hacker.find().then(rec => {
     if(rec) {
@@ -63,6 +64,59 @@ router.delete('/hackers/:id', function(req, res) {
     }
     res.send(rec);
   })
-})
+});
+
+var clubhousefacade_transporter = getTransporter($env.EMAIL_CLUBHOUSEFACADE ,$env.PASSWORD_CLUBHOUSEFACADE);
+//  credentials 
+function getTransporter(email ,password)  {
+  let transporter=  nodemailer.createTransport({
+       service: 'gmail',
+       auth: {
+         user: email,
+         pass: password
+       }});
+       return transporter;
+ }
+ function getMailOptions({email ,to ,fname,lname, subject, message,phonenumber} ) {
+  let mailOptions = {
+      from:email,
+      // organisation Email 
+      to,
+      subject:`Hi Clubhouse Facade ----${subject}--`,
+      text:message,
+      phonenumber,
+      html: `<h1>${message}</h1><p>That was easy! name ${fname} ${lname} ${phonenumber}</p>`
+
+  };
+    return mailOptions;
+    
+}
+
+
+
+router.post("/clubhousefacadeemailservice", function(req, res) {
+  let resBody = {
+      fname :req.body.fname,
+      lname :req.body.lname,
+      email :req.body.email,
+      phonenumber :req.body.phonenumber,
+      to : $env.EMAIL_CLUBHOUSEFACADE,
+      subject :req.body.subject,
+      message :req.body.message,
+  }
+  console.log(req.body);
+  console.log(resBody);
+  var clubhousefacade_mailOptions = getMailOptions(resBody);
+
+  clubhousefacade_transporter.sendMail(clubhousefacade_mailOptions, function(error, info){
+      if (error) {
+        console.log(error);
+      } else {
+        console.log('Email sent: ' + info.response);
+        res.send("Email Sent Successfully");
+      }
+    });
+  console.log(`${port} is working`);
+});
 
 module.exports = router;
